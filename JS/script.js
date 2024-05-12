da4ee2ab3d1928ed23ba25799ee0543a19f0c1b2
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-12 17:02:13 +0200

    Creato ciclo for con if al suo interno per stampare in console ,quando dividendo per 3,5, 3 e 5 il resto é uguale a 0,  Fizz, Buzz, FizzBuzz a seconda dei casi

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,14 @@
-console.log('Hello!')
+for (let i=1; i < 100; i = i + 1){
+    if (i % 5 === 0 && i % 3 === 0){
+        console.log ('FizzBuzz')
+    }
+    else if(i % 3 === 0){
+        console.log ('Fizz')
+    }
+    else if (i % 5 === 0){
+        console.log ('Buzz')
+    }
+    else{
+        console.log (i)
+    }
+}
